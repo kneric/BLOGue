ca83a2bf8511e40b782633fa8f8605f2f0ffe4fd
kneric <brian.pradipta.a@gmail.com>
2018-08-26 08:38:16 +0700

    create vue instance for axios

--- blogue/src/main.js
+++ blogue/src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios.create(
+  {
+    baseURL: 'http://localhost:3000'
+  }
+)
 
 Vue.config.productionTip = false
 
